a8c9e66ff164b8d8272c72731b1064026c87be20
Bosco Vallejo-Nágera <bosco@vallejonagera.xyz>
2023-12-02 19:57:10 +0100

    two

--- main.go
+++ main.go
@@ -10,45 +10,53 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	const redAmount = 12
-	const greenAmount = 13
-	const blueAmount = 14
+	// const redAmount = 12
+	// const greenAmount = 13
+	// const blueAmount = 14
 	sum := 0
 
-	scanLoop:
+scanLoop:
 	for scanner.Scan() {
 		text := scanner.Text()
 		if text == "" {
 			break
 		}
 		s := strings.Split(text, ": ")
-		index := strings.Split(s[0], " ")
-		num, err := strconv.Atoi(index[1])
-		if err != nil {
-			break
-		}
+		// index := strings.Split(s[0], " ")
+		// num, err := strconv.Atoi(index[1])
+		// if err != nil {
+		// 	break
+		// }
 		games := strings.Split(s[1], "; ")
-		for _, game := range(games){
+		m := map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+		for _, game := range games {
 			balls := strings.Split(game, ", ")
 			if len(balls) <= 3 {
-				m:= make(map[string]int)
-				for _, ball := range(balls) {
+				for _, ball := range balls {
 					amountBall := strings.Split(ball, " ")
 					amount, err := strconv.Atoi(amountBall[0])
 					if err != nil {
 						continue scanLoop
 					}
-					m[amountBall[1]] = amount
+					m[amountBall[1]] = max(amount, m[amountBall[1]])
 				}
-				if m["red"] > redAmount || m["green"] > greenAmount || m["blue"] > blueAmount {
-					continue scanLoop
-				}
-			} else {
-				continue scanLoop
+				// if m["red"] > redAmount || m["green"] > greenAmount || m["blue"] > blueAmount {
+				// 	continue scanLoop
+				// }
+			} 
+		}
+		power := 1
+		for k, v := range(m) {
+			fmt.Println(k)
+			if v >= 0 {
+				power = power * v
 			}
 		}
-		sum += num
-
+		sum += power
 
 	}
 
